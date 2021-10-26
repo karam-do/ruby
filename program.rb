537d11693d4d1611afe78f1b1edddfaaae8eba58
Karam Doubbaneh <karam.doubbaneh@vanderlande.com>
2021-10-26 14:33:20 +0200

    added a loop

--- program.rb
+++ program.rb
@@ -5,6 +5,7 @@ height = "7.3"
 puts ("Hey my Name is " + name)
 puts ("And I am " + age + " years old")
 puts ("The Weather on a height of " + height + " meter is really goood!")
-puts ("Whats your name? ")
-ur_name = gets
-puts ("Hello " + ur_name)
+loop do
+    puts Time.now
+	sleep (5)
+end
